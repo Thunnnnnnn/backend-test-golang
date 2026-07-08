5e7fd9f524d0087c60a2a3feafed1df6d462b555
Thun Wityakon <thunwi@hotmail.com>
2026-07-09 01:21:58 +0700

    Setup godotenv

--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"backend-test-golang/database"
+	"log"
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Create a Gin router with default middleware (logger and recovery)
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowAllOrigins: true,
+		AllowMethods: []string{
+			"GET",
+			"POST",
+			"PUT",
+			"DELETE",
+			"OPTIONS",
+		},
+		AllowHeaders: []string{
+			"*",
+		},
+	}))
+
+	if err := database.ConnectMongo(); err != nil {
+		log.Fatal("Mongo connection failed:", err)
+	}
 
 	// Define a simple GET endpoint
 	r.GET("/ping", func(c *gin.Context) {
